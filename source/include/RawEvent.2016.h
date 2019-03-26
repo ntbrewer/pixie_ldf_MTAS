48144c885dddcf0c08467ce55e82b2637e995b7b
Nathan Brewer <brewer.nathant@gmail.com>
2019-03-26 16:20:11 -0500

    Update for ANL March
    General bookeeping and neatness updates + extra ReadBuff files

--- source/include/RawEvent.2016.h
+++ source/include/RawEvent.2016.h
@@ -46,19 +46,16 @@ class Identifier {
     int dammID;       /**< Damm spectrum number for plotting calibrated energies */
     int location;     /**< Specifies the real world location of the channel.
 			 For the DSSD this variable is the strip number */
-		int modNum;       /**< Specifies the Pixie Module for the channel*/
  public:
     void SetDammID(int a)     {dammID = a;}   /**< Set the dammid */
     void SetType(string a)    {type = a;}     /**< Set the detector type */
     void SetSubtype(string a) {subtype = a;}  /**< Set the detector subtype */
     void SetLocation(int a)   {location = a;} /**< Set the detector location */
-    void SetModNum(int a)     {modNum = a;}    /**< Set the detector module number*/
     
     int GetDammID() const     {return dammID;}   /**< Get the dammid */
     string GetType() const    {return type;}     /**< Get the detector type */
     string GetSubtype() const {return subtype;}  /**< Get the detector subtype */
     int GetLocation() const   {return location;} /**< Get the detector location */
-    int GetModNum() const {return modNum;}    /**< Set the detector module number*/
 
     Identifier();
     void Zero();
@@ -90,7 +87,6 @@ class ChanEvent {
     unsigned long trigTime;    /**< The channel trigger time, trigger time and the lower 32 bits
 				     of the event time are not necessarily the same but could be
 				     separated by a constant value.*/
-    unsigned long cfdTime;     /**< CFD trigger time in units of 1/256 pixie clock ticks */
     unsigned long eventTimeLo; /**< Lower 32 bits of pixie16 event time */
     unsigned long eventTimeHi; /**< Upper 32 bits of pixie16 event time */
     unsigned long runTime0;    /**< Lower bits of run time */
@@ -101,10 +97,6 @@ class ChanEvent {
     int    modNum;             /**< Module number */
     int    chanNum;            /**< Channel number */
 
-    bool   virtualChannel;     /**< Flagged if generated virtually in Pixie DSP */
-    bool   pileupBit;          /**< Pile-up flag from Pixie */
-    bool   saturatedBit;       /**< Saturation flag from Pixie */
-    double kTemperature;
     void ZeroNums(void);       /**< Zero members which do not have constructors associated with them */
     
     // make the front end responsible for reading the data able to set the channel data directly
@@ -119,33 +111,15 @@ class ChanEvent {
     void SetTime(double a)      {time = a;}      /**< Set the raw time */
     void SetCalTime(double a)   {calTime = a;}   /**< Set the calibrated time */
     void AddTraceInfo(double a) {traceInfo.push_back(a);} /**< Add one value to the traceinfo */
-    vector<double> GetTraceInfo() const {return traceInfo;} 
+
     double GetEnergy() const      {return energy;}      /**< Get the raw energy */
     double GetCalEnergy() const   {return calEnergy;}   /**< Get the calibrated energy */
     double GetTime() const        {return time;}        /**< Get the raw time */
     double GetCalTime() const     {return calTime;}    /**< Get the calibrated time */
     const vector<int> &GetTraceRef() const {return trace;} /**< Get a reference to the trace */
-  /*NTB to be adapted from Trace.hpp 2012
-    needs     std::map<std::string, double> doubleTraceData;
-    std::map<std::string, int>    intTraceData;
-    and #include <map>
-    void SetTraceInfo(std::string name, double value) {
-        if (traceInfo.count(name) > 0) 
-            traceInfo[name] = value;
-        else
-            InsertTraceInfo(name,value);
-    }
-    void InsertTraceInfo(std::string name, double value) {
-        traceInfo.insert(make_pair(name,value));
-    }
-    bool HasTraceInfo(std::string name) const {
-        return (traceInfo.count(name) > 0 );
-    } */
 
     unsigned long GetTrigTime() const    
 	{return trigTime;}    /**< Return the channel trigger time */
-    unsigned long GetCfdTime() const    
-	{return cfdTime;}    /**< Return the CFD trigger time */
     unsigned long GetEventTimeLo() const
 	{return eventTimeLo;} /**< Return the lower 32 bits of event time */
     unsigned long GetEventTimeHi() const
@@ -156,46 +130,30 @@ class ChanEvent {
 	{return runTime1;}    /**< Return the middle bits of run time */
     unsigned long GetRunTime2() const
 	{return runTime2;}    /**< Return the higher bits of run time */
-    bool IsPileup() const 
-  {return pileupBit;}
-    bool IsSaturated() const 
-  {return saturatedBit;}
 
     const Identifier& GetChanID() const; /**< Get the channel identifier */
     int GetID() const;                   /**< Get the channel id defined as
 					    pixie module # * 16 + channel number */
     double GetTraceInfo(unsigned int a) const; /**< Get a specific value from the traceinfo */
 
-/****Added for SVP / NTB ****/
-    double TrcQDC;
-    int MaxValue,SatLen;
-    double StdDevBaseline,AveBaseline, AveBaselinePost;
-    int Phase; 
+/****Added for SVP ****/
+    double TrcQDC, MaxValue;
+    double StdDevBaseline, Phase, AveBaseline; 
     unsigned int MaxPos;
-    int tRegion;
-
-    void SetSatLen(int a) {SatLen=a;}       /**< Set the number of Saturated Channels NTB*/
+    
     void SetTrcQDC(double a) {TrcQDC=a;}       /**< Set the QDC From Integrating The Trace*/
     void SetMaxValue(double a)  {MaxValue=a;}        /**< Set the Maximum Value from Trace*/
     void SetStdDevBaseline(double a) {StdDevBaseline=a;}       /**< Set the Standard Deviation of the Baseline the Trace*/
     void SetAveBaseline(double a)  {AveBaseline=a;}        /**< Set the Average of the baseline from Trace*/
-    void SetAveBaselinePost(double a)  {AveBaselinePost=a;}        /**< Set the Average of the Post Pulse baseline from Trace*/
-    void SetPhase(int a)    {Phase=a;}          /**< Set the Phase from Trace*/
+    void SetPhase(double a)    {Phase=a;}          /**< Set the Phase from Trace*/
     void SetMaxPos(unsigned int a) {MaxPos=a;}           /**< Set the Position of the Maximum from Trace */
-    void SetRegion(int region) {tRegion=region;}  /**Set the Position of the Region defined in Trace */ 
-    void SetTemperature(double a) {kTemperature=a;} /**Set the Temperature in K defined in ThermometerProcessor */ 
     
-    int GetSatLen() const {return SatLen;}       /**< Return the Maximum Value from Trace*/
     double GetTrcQDC() const {return TrcQDC;} /**< Return the QDC From Integrating The Trace*/
-    int GetMaxValue() const {return MaxValue;}       /**< Return the Maximum Value from Trace*/
+    double GetMaxValue()   {return MaxValue;}       /**< Return the Maximum Value from Trace*/
     double GetStdDevBaseline() const {return StdDevBaseline;} /**< Return the Standard Deviation from  The Trace*/
-    double GetAveBaseline() const {return AveBaseline;}           /**< Return the Average of the Baseline from Trace*/
-    double GetAveBaselinePost() const {return AveBaselinePost;}           /**< Return the Average of the Baseline from Trace*/
-    int GetPhase() const {return Phase;}       /**< Return the phase from Trace*/
-    double GetHighResTime() const {return time + Phase;}  /**< Return the time + phase*/
-    unsigned int GetMaxPos() const {return MaxPos;} /**< Return position of Maximum in trace */
-    int GetRegion() const {return tRegion;}
-    double GetTemperature() const {return kTemperature;}
+    double GetAveBaseline()   {return AveBaseline;}           /**< Return the Average of the Baseline from Trace*/
+    double GetPhase()   {return Phase;}       /**< Return the phase from Trace*/
+    unsigned int GetMaxPos() {return MaxPos;}        /**< Return position of Maximum in trace */
 /****END SVP ADDITIONS****/
 
     ChanEvent();
